b7e51171ac9a8cb4bb8eb1a8c096fd0b83d25631
Rafał Borcz <RafalBO99@outlook.com>
2019-08-26 22:37:54 +0200

    Migrate to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+buildscript {
+    val kotlinVersion by rootProject.extra { "1.3.50" }
+
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.5.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.0-alpha01")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
